ed75a382321e0b8291345cb7c6ef017a12662434
Andy Dill <andy.dill@gmail.com>
2020-06-15 13:05:24 -0700

    feat: support drag & drop for offscreen webcontents

--- shell/browser/api/electron_api_web_contents.h
+++ shell/browser/api/electron_api_web_contents.h
@@ -20,6 +20,7 @@
 #include "content/public/browser/render_widget_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_observer.h"
+#include "content/public/common/drop_data.h"
 #include "electron/buildflags/buildflags.h"
 #include "electron/shell/common/api/api.mojom.h"
 #include "gin/handle.h"
@@ -423,6 +424,13 @@ class WebContents : public gin::Wrappable<WebContents>,
     return script_executor_.get();
   }
 #endif
+
+  bool start_dragging = false;
+  bool dragging = false;
+  content::DropData drop_data;
+  blink::DragOperationsMask drag_ops = blink::kDragOperationNone;
+  gfx::ImageSkia drag_image;
+  gfx::Vector2d drag_image_offset;
 
  protected:
   // Does not manage lifetime of |web_contents|.
